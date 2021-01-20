656f905dda09ad7bff6f9cdb3595fc9090663374
Burak Can Kaya <burak.kaya24@ogr.sakarya.edu.tr>
2021-01-20 20:52:15 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ID + " " + musteri.isim + " " + musteri.yas +" Bilgileri Eklenmiştir.");
+        }
+        public void MusteriListeleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ID + " " + musteri.isim + " " + musteri.yas + " Bilgileri Listelenmiştir.");
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ID + " " + musteri.isim + " " + musteri.yas + " Bilgileri Silinmiştir.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri mstr1 = new Musteri();
+
+            mstr1.ID = 1000;
+            mstr1.isim = "Burak";
+            mstr1.yas = 18;
+
+            Musteri mstr2 = new Musteri();
+
+            mstr2.ID = 2000;
+            mstr2.isim = "Can";
+            mstr2.yas = 18;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(mstr1);
+            musteriManager.MusteriEkle(mstr2);
+            musteriManager.MusteriListeleme(mstr1);
+            musteriManager.MusteriSilme(mstr2);
+
+        }
+    }
+}
